dcd37cc539cebe048e91fd8cc5661aefe8a4f20a
Nicolas Alancay <nico.alan.alb@gmail.com>
2023-09-16 12:39:04 -0300

    Clase Cliente para persistir datos en la tabla "cliente" de la BBDD.
    Se usan las annotations:
    
    _ Entity: para representar una tabla en la BBDD. Permite que las
    instancias de esta clase se mapeen a una fila de la tabla.
    
    _ Table: para indicar el nombre de la tabla de la BBDD a la que se mapea
    la entidad.
    
    _ Column: para configurar los mapeos de un atributo de la clase a una
    columna de la BBDD.
    
    _ Id: para indicar que un atributo de la clase es PK de la entidad.

---
+++ src/main/java/com/nico/conexionHibernate/Cliente.java
@@ -0,0 +1,72 @@
+package com.nico.conexionHibernate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "cliente")
+public class Cliente {
+
+	@Id
+	@Column(name = "id")
+	private int id;
+
+	@Column(name = "nombre")
+	private String nombre;
+
+	@Column(name = "apellido")
+	private String apellido;
+
+	@Column(name = "direccion")
+	private String direccion;
+
+	public Cliente() {
+
+	}
+
+	public Cliente(String nombre, String apellido, String direccion) {
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.direccion = direccion;
+	}
+
+	@Override
+	public String toString() {
+		return "Cliente [id=" + id + ", nombre=" + nombre + ", apellido=" + apellido + ", direccion=" + direccion + "]";
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public String getDireccion() {
+		return direccion;
+	}
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+
+}
